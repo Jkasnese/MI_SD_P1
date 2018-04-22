58bb0d550b3a31733bcd5f4c641b82b673066d25
Jkasnese <guilhermelw@gmail.com>
2018-04-22 18:44:42 -0300

    Added nios project. Unpinned uncompiled

---
+++ nios_project/unsaved/unsaved_bb.v
@@ -0,0 +1,9 @@
+module unsaved (
+	clk_clk,
+	btn_external_connection_export,
+	leds_external_connection_export);	
+
+	input		clk_clk;
+	input		btn_external_connection_export;
+	output	[7:0]	leds_external_connection_export;
+endmodule
---
+++ nios_project/unsaved/unsaved_inst.v
@@ -0,0 +1,6 @@
+	unsaved u0 (
+		.clk_clk                         (<connected-to-clk_clk>),                         //                      clk.clk
+		.btn_external_connection_export  (<connected-to-btn_external_connection_export>),  //  btn_external_connection.export
+		.leds_external_connection_export (<connected-to-leds_external_connection_export>)  // leds_external_connection.export
+	);
+
